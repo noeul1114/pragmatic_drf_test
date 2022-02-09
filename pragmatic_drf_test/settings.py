318170023e27e5f62daa71594db7b53b304e1dd5
Park HS <parkhyongsok@naver.com>
2022-02-09 18:45:43 +0900

    DRF default authentication settings update

--- pragmatic_drf_test/settings.py
+++ pragmatic_drf_test/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
+
 # Application definition
 
 INSTALLED_APPS = [
