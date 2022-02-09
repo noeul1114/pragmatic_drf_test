c451f5059cb743448e8292eeee1af2b69d1de28d
Park HS <parkhyongsok@naver.com>
2022-02-09 18:45:23 +0900

    Serializer password set error fixed

--- accountapp/serializers.py
+++ accountapp/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
-from rest_framework.serializers import ModelSerializer, HyperlinkedModelSerializer
+from rest_framework.serializers import ModelSerializer, HyperlinkedModelSerializer, raise_errors_on_nested_writes
+from rest_framework.utils import model_meta
 
 from accountapp.models import PragmaticUser
 
@@ -19,3 +20,35 @@ class PragmaticUserSerializer(HyperlinkedModelSerializer):
 
     def create(self, validated_data):
         return self.Meta.model.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        raise_errors_on_nested_writes('update', self, validated_data)
+        info = model_meta.get_field_info(instance)
+
+        # Simply set each attribute on the instance, and then save it.
+        # Note that unlike `.create()` we don't need to treat many-to-many
+        # relationships as being a special case. During updates we already
+        # have an instance pk for the relationships to be associated with.
+        m2m_fields = []
+        for attr, value in validated_data.items():
+            if attr in info.relations and info.relations[attr].to_many:
+                m2m_fields.append((attr, value))
+            else:
+                setattr(instance, attr, value)
+
+        if validated_data.get('password') is not None:
+            instance.set_password(validated_data['password'])
+
+        instance.save()
+
+        # Note that many-to-many fields are set after updating instance.
+        # Setting m2m fields triggers signals which could potentially change
+        # updated instance and we do not want it to collide with .update()
+        for attr, value in m2m_fields:
+            field = getattr(instance, attr)
+            field.set(value)
+
+        return instance
+
+
+
